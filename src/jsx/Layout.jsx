2e0995303049e0c9009b87e70098e01596274782
TomaszOpalka <to31144@gmail.com>
2025-03-25 23:50:09 +0100

    pomidorowy commit importy cz dalsza

--- src/jsx/Layout.jsx
+++ src/jsx/Layout.jsx
@@ -1,5 +1,5 @@
 import "react";
-import Header from "./Header.jsx";
+import Header from "../jsx/Header.jsx";
 import Hero from "./Hero.jsx";
 import Statistic from "./Statistic.jsx";
 import Steps from "./Steps.jsx";
