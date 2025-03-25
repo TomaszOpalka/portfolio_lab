24b5e2c90e3f1308b1cf70c3a2f6e34cea5dfd82
TomaszOpalka <to31144@gmail.com>
2025-03-25 22:11:21 +0100

    aaaa222

--- src/jsx/CreateAcc.jsx
+++ src/jsx/CreateAcc.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Header from './Header.jsx';
+// import Header from "./Header.jsx";
+import Header from "../Header.jsx";
 import { supabase } from './Client.jsx';
 import { Link } from 'react-router-dom';
 
