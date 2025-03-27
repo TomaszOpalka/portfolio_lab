dd148277667ebfe86c220d2b7ab9f6a5587b07b1
TomaszOpalka <to31144@gmail.com>
2025-03-27 19:13:52 +0100

    dodawanie importów fotek Hero test

--- src/jsx/Hero.jsx
+++ src/jsx/Hero.jsx
@@ -2,25 +2,26 @@ import { Link } from 'react-router-dom';
 import HomeHeroImage from '../assets/Home-Hero-Image.jpg';
 import Decoration from '../assets/Decoration.svg';
 
-
-
-function Hero(){
-
-    return(
+function Hero() {
+    return (
        <>
         <div className="home" id="hero"> 
             <div className="hero-img">
-                <img className="home-img" src='src/assets/Home-Hero-Image.jpg' ></img>
+                <img className="home-img" src={HomeHeroImage} alt="Hero" />
             </div>
             <div className="hero">
                 <div className="hero-h1">
                     <h1> Zacznij pomagać! </h1>
                     <h1> Oddaj niechciane rzeczy w zaufane ręce</h1>
                 </div>
-                    <img className="hero-decoration" src="src\assets\Decoration.svg"></img>
+                <img className="hero-decoration" src={Decoration} alt="Decoration" />
                 <div className="hero-btns">
-                <Link to="/login"><button className="hero-btn">ODDAJ<br></br>RZECZY</button></Link>
-                <Link to="/login"><button className="hero-btn">ZORGANIZUJ ZBIÓRKĘ</button></Link>
+                    <Link to="/login">
+                        <button className="hero-btn">ODDAJ<br />RZECZY</button>
+                    </Link>
+                    <Link to="/login">
+                        <button className="hero-btn">ZORGANIZUJ ZBIÓRKĘ</button>
+                    </Link>
                 </div>
             </div>
         </div>
